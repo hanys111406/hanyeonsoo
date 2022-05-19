d434fef012dbfabe23edc8444aa8193d1da83293
hanys111406 <hanys111406@naver.com>
2022-05-19 20:32:16 +0900

    adsfasdsf

--- lib/src/first.dart
+++ lib/src/first.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../main.dart';
 
 class first extends StatelessWidget {
   const first({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 120, height: 60,
+                color: Colors.red,
+              ),
+              Container(
+                width: 120, height: 60,
+                color: Colors.blue,
+              ),
+                SizedBox(height: 100,),
+                RaisedButton(
+                  child: Text('Go to First page'),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => MyApp()));
+                  },
+              ),
+            ],
+          ),
+        ),
+    );
   }
 }
